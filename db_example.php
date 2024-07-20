3349db7024b2bdfd66883ec3bbede477ab953552
Daniil Gentili <daniil@daniil.it>
2024-07-20 19:56:35 +0200

    Bump

--- db_example.php
+++ db_example.php
@@ -3,7 +3,7 @@
 use Amp\Mysql\MysqlConfig;
 
 return new MysqlConfig(
-    host: 'unix_socket=/var/run/mysqld/mysqld.sock',
+    host: '/var/run/mysqld/mysqld.sock',
     user: 'user',
     password: 'pass',
     database: 'rpc',
